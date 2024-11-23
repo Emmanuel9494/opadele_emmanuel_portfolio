3db4121dd9270a742ea4286b244fa48d85743b64
Emmanuel9494 <144351016+Emmanuel9494@users.noreply.github.com>
2024-11-22 20:06:54 -0500

    Mobile Structure Done
    Mobile Structure Done

--- js/main.js
+++ js/main.js
@@ -250,3 +250,32 @@ skills(xpCount);
 
 })();
 
+// about Tabs
+(function () {
+  const tabs = document.querySelectorAll(".abt-tab");
+
+  tabs.forEach((tab) => {
+      const head = tab.querySelector(".abt-head");
+
+      head.addEventListener("click", () => {
+      tab.classList.toggle("active");
+      
+      const note = tab.querySelector(".abt-note");
+          if (tab.classList.contains("active")) {
+              note.style.display = "block";
+
+              gsap.from(note, 
+                { opacity: 1, 
+                  y: -10, 
+                  duration: 1});
+          } else {
+              note.style.display = "none"; 
+          }
+      });
+  });
+  
+
+
+
+
+})();
