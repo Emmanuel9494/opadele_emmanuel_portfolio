8f8c5a1006d0f2529d450f53a1f0d1ca05045214
Emmanuel9494 <144351016+Emmanuel9494@users.noreply.github.com>
2024-11-26 04:21:36 -0500

    Pages Complete
    Index, Work, and Intro Pages done.

--- js/main.js
+++ js/main.js
@@ -1,3 +1,8 @@
+import { introAnimation } from './animation.js';
+
+introAnimation();
+
+
 (function () {
   // Check if the current page is 'index.html'
   if (window.location.pathname.includes("index.html") || (window.location.pathname.includes("works.html"))){
@@ -317,3 +322,379 @@ selectors.forEach((selector, i) => {
 });
 
 })();
+
+// Animations and Scroll Triggers index page
+(function () {
+    if (window.location.pathname.includes("index.html")){
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.registerPlugin(ScrollToPlugin);
+        const tl = gsap.timeline();
+
+        tl.fromTo(
+          "#intro-text",
+          { opacity: 0 },
+          { opacity: 1, duration: .5, ease: "power2.out" }
+        );
+        
+        tl.fromTo(
+          ".intro-bio",
+          { opacity: 0, y: 100 },
+          { opacity: 1, duration: 1, y: 0, ease: "power2.out" }
+        );
+        tl.fromTo(
+            ".cta-work",
+            { opacity: 0, y: 100 },
+            { opacity: 1, duration: 1, y: 0, ease: "power2.out" }
+          );
+        //   Image Galleery Slider
+        gsap.from("#slider-container", {
+            scrollTrigger: {
+              trigger: "#slider-container",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false 
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "power1.out"
+          });
+        //   text Switching
+        gsap.from("#index-text-switch", {
+            scrollTrigger: {
+              trigger: "#index-text-switch",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "power1.out"
+          });
+          
+          //   Image Galleery Slider2
+        gsap.from("#slider-container2", {
+            scrollTrigger: {
+              trigger: "#slider-container2",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false 
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "power1.out"
+          });
+        //   Experience animation
+        const tl2 = gsap.timeline({
+            scrollTrigger: {
+              trigger: ".xp-head",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false
+            }
+          });
+          
+          tl2.fromTo(
+            ".xp-head",
+            { opacity: 0 },
+            { opacity: 1, duration: 1, ease: "power3.out" }
+          );
+          
+          tl2.fromTo(
+            ".xp-sub-head",
+            { opacity: 0, x: 100 },
+            { opacity: 1, duration: .5, x: 0, ease: "power3.out" }
+          );
+          
+          tl2.fromTo(
+            ".xp-note-container",
+            { opacity: 0, y: 100 },
+            { opacity: 1, duration: 1, y: 0, ease: "expo.out" }
+          );
+ //   Works Completed
+        gsap.from(".mini-works", {
+            scrollTrigger: {
+              trigger: ".mini-works",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "power1.out"
+          });
+          //   About Section
+        gsap.from("#main-about", {
+            scrollTrigger: {
+              trigger: "#main-about",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "power1.out"
+          });
+           //   Beyond Note
+        gsap.from(".beyond-ad", {
+            scrollTrigger: {
+              trigger: "#beyond",
+              start: "40% 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "power1.out"
+          });
+           //   P1 and P2
+        const tl3 = gsap.timeline({
+            scrollTrigger: {
+              trigger: ".trig-hr",
+              start: "30% 60%",
+              toggleActions: "play reverse play reverse",
+              markers: false
+            }
+          });
+          tl3.fromTo(
+            ".trig-hr",
+            { opacity: 0, height:0 },
+            { opacity: 1, duration: 1, height:165 ,ease: "power3.out" }
+          );
+          tl3.fromTo(
+            ".trig-logo",
+            { opacity: 0},
+            { opacity: 1, duration: 1, ease: "power3.out" }
+          );
+          
+          tl3.fromTo(
+            ".p1",
+            { opacity: 0, x: -100 },
+            { opacity: 1, duration: 1,x: 0, ease: "power3.out" }
+          );
+          
+          tl3.fromTo(
+            ".p2",
+            { opacity: 0, x: 100 },
+            { opacity: 1, duration: 1, x: 0, ease: "power3.out" }
+          );
+    }
+})();
+// Animations and Scroll Triggers Works page
+(function () {
+    if (window.location.pathname.includes("works.html")){
+        gsap.registerPlugin(ScrollTrigger);
+        gsap.registerPlugin(ScrollToPlugin);
+        const tl = gsap.timeline();
+
+        tl.fromTo(
+          ".comp-head",
+          { opacity: 0 },
+          { opacity: 1, duration: .5, ease: "power2.out" }
+        );
+        
+        tl.fromTo(
+          ".comp-title",
+          { opacity: 0, y: 100 },
+          { opacity: 1, duration: .5, y: 0, ease: "back.out(1.7)" }
+        );
+        tl.fromTo(
+            ".comp-sub-title",
+            { opacity: 0, y: 100 },
+            { opacity: 1, duration: .5, y: 0, ease: "power2.out" }
+          );
+          tl.fromTo(
+            ".comp-sub-head",
+            { opacity: 0, y: 100 },
+            { opacity: 1, duration: .5, y: 0, ease: "back.out(1.7)" }
+          );
+          tl.fromTo(
+            ".comp-date",
+            { opacity: 0, y: 100 },
+            { opacity: 1, duration: .5, y: 0, ease: "power2.out" }
+          );
+          tl.fromTo(
+            ".comp-cta",
+            { opacity: 0, y: 100 },
+            { opacity: 1, duration: .5, y: 0, ease: "back.out(1.7)" }
+          );
+        //   Image Galleery Slider
+        gsap.from(".ws1", {
+            scrollTrigger: {
+              trigger: ".ws1",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false 
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "power1.out"
+          });
+            //   Project Info
+        gsap.from("#p-info", {
+            scrollTrigger: {
+              trigger: "#p-info",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false 
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "back.out(1.7)"
+          });
+            //   Show Image
+        gsap.from(".ws2", {
+            scrollTrigger: {
+              trigger: ".ws2",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false 
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "back.out(1.7)"
+          });
+       
+          
+         
+        //   Experience animation
+        const tl2 = gsap.timeline({
+            scrollTrigger: {
+              trigger: ".xp-head",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false
+            }
+          });
+          
+          tl2.fromTo(
+            ".xp-head",
+            { opacity: 0 },
+            { opacity: 1, duration: 1, ease: "power3.out" }
+          );
+          
+          tl2.fromTo(
+            ".xp-sub-head",
+            { opacity: 0, x: 100 },
+            { opacity: 1, duration: .5, x: 0, ease: "power3.out" }
+          );
+          
+          tl2.fromTo(
+            ".xp-note-container",
+            { opacity: 0, y: 100 },
+            { opacity: 1, duration: 1, y: 0, ease: "expo.out" }
+          );
+ //   Works Completed
+        gsap.from(".mini-works", {
+            scrollTrigger: {
+              trigger: ".mini-works",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "power1.out"
+          });
+          //   About Section
+        gsap.from("#main-about", {
+            scrollTrigger: {
+              trigger: "#main-about",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "power1.out"
+          });
+           //   Beyond Note
+        gsap.from(".beyond-ad", {
+            scrollTrigger: {
+              trigger: "#beyond",
+              start: "40% 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "power1.out"
+          });
+            //   Beyond notes
+        gsap.from(".b-notes", {
+            scrollTrigger: {
+              trigger: ".b-notes",
+              start: "top 80%",
+              toggleActions: "play reverse play reverse",
+              markers: false 
+            },
+            opacity: 0,
+            y: 100,
+            duration: 1,
+            ease: "back.out(1.7)"
+          });
+           //   P1 and P2
+        const tl3 = gsap.timeline({
+            scrollTrigger: {
+              trigger: ".trig-hr",
+              start: "30% 60%",
+              toggleActions: "play reverse play reverse",
+              markers: false
+            }
+          });
+          tl3.fromTo(
+            ".trig-hr",
+            { opacity: 0, height:0 },
+            { opacity: 1, duration: 1, height:165 ,ease: "power3.out" }
+          );
+          tl3.fromTo(
+            ".trig-logo",
+            { opacity: 0},
+            { opacity: 1, duration: 1, ease: "power3.out" }
+          );
+          
+          tl3.fromTo(
+            ".p1",
+            { opacity: 0, x: -100 },
+            { opacity: 1, duration: 1,x: 0, ease: "power3.out" }
+          );
+          
+          tl3.fromTo(
+            ".p2",
+            { opacity: 0, x: 100 },
+            { opacity: 1, duration: 1, x: 0, ease: "power3.out" }
+          );
+    }
+})();
+// Scroll Links
+(() => {
+
+    gsap.registerPlugin(ScrollTrigger);
+    gsap.registerPlugin(ScrollToPlugin);
+
+    const navLinks = document.querySelectorAll(" #head-nav .link-scroll");
+
+    function scrollLink(e) {    
+            e.preventDefault(); 
+            console.log(e.currentTarget.hash);
+            let selectedLink = e.currentTarget.hash;
+            gsap.to(window, {duration: 1, scrollTo:{y:`${selectedLink}`, offsetY:100 }});
+    }
+
+    navLinks.forEach((link) => {
+        link.addEventListener("click", scrollLink);
+    });
+  
+})();
+// Nothing comes After line 700...its time to push and go head on with PHP. 
